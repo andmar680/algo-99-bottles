d4e8d5e2fedc12996b5cc9d7075bd470cb812ad8
andmar680 <andmar680@gmail.com>
2023-02-06 16:20:39 -0600

    first attempted answer

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,19 @@
-function bottleSong() {
-  // code goes here
+function bottleSong(amountOfBeers) {
+ 
+
+  if (amountOfBeers === 1) {
+    return `1 bottle of beer on the wall, 1 bottle of beer.
+      Take one down and pass it around, no more bottles of beer on the wall.
+      No more bottles of beer on the wall, no more bottles of beer 😢.
+      Go to the store and buy some more, 99 bottles of beer on the wall 🙂.`
+  } else {
+    console.log(`${amountOfBeers} bottles of beer on the wall, ${amountOfBeers} bottles of beer. 
+    Take one down and pass it around, ${amountOfBeers - 1} bottles of beer on the wall.`)
+    return bottlesOfBeerSong(amountOfBeers - 1)
+  }
+}
+
+console.log(bottlesOfBeerSong(99))
 };
 
 bottleSong();
