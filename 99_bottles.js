b91141abfe973bca1a4e74e70bd0265e0cac031f
andmar680 <andmar680@gmail.com>
2023-02-07 08:24:26 -0600

    spelling error corrected

--- 99_bottles.js
+++ 99_bottles.js
@@ -3,13 +3,20 @@ function bottleSong(amountOfBeers) {
   if (amountOfBeers === 1) {
     return `1 bottle of beer on the wall, 1 bottle of beer.
       Take one down and pass it around, no more bottles of beer on the wall.
-      No more bottles of beer on the wall, no more bottles of beer 😢.
-      Go to the store and buy some more, 99 bottles of beer on the wall 🙂.`
-  } else {
+      No more bottles of beer on the wall, no more bottles of beer.
+      Go to the store and buy some more, 99 bottles of beer on the wall.`
+  } else if (amountOfBeers === 2) {
+    console.log(`${amountOfBeers} bottles of beer on the wall, ${amountOfBeers} bottles of beer.
+      Take one down and pass it around, ${amountOfBeers - 1} more bottle of beer on the wall.`)
+      return bottleSong(amountOfBeers - 1)
+  }
+  
+  
+  else {
     console.log(`${amountOfBeers} bottles of beer on the wall, ${amountOfBeers} bottles of beer. 
     Take one down and pass it around, ${amountOfBeers - 1} bottles of beer on the wall.`)
-    return bottlesOfBeerSong(amountOfBeers - 1)
+    return bottleSong(amountOfBeers - 1)
   }
 }
 
-console.log(bottleSong(99));
+console.log(bottleSong(3));
